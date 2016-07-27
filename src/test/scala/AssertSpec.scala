6897bae70cc9552a266b3747f69b3fbbdf1d2912
ronnie-o <sungmin.ooh@gmail.com>
2016-07-27 14:54:06 +0900

    Assert

--- src/test/scala/AssertSpec.scala
+++ src/test/scala/AssertSpec.scala
@@ -8,4 +8,23 @@ class AssertSpec extends FunSuite with Matchers {
   test("hello scala") {
     println("world")
   }
+  
+  test("assert") {
+    val left = 2
+    val right = 1
+    assert(left != right)
+  }
+  
+  test("boolean") {
+    true shouldBe true
+  }
+  
+  test("number") {
+    val v1 = 4
+    v1 shouldBe 4
+  }
+  
+  test("value") {
+    assert(2 == 1 + 1)
+  }
 }
