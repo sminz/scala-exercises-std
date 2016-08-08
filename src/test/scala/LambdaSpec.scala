28c50ac17d6d383cee15d6c52daa73166d7186f3
ronnie-o <sungmin.ooh@gmail.com>
2016-08-08 14:43:06 +0900

    high order functions

--- src/test/scala/LambdaSpec.scala
+++ src/test/scala/LambdaSpec.scala
@@ -23,4 +23,19 @@ class LambdaSpec extends FunSuite with Matchers {
     f3(100) shouldBe "100"
     f4(1000) shouldBe "1000"
   }
+  
+  test("int to string") {
+    def myHighOrderFunc(f : Int => String)(v : Int) = f(v)
+    
+    myHighOrderFunc(x => x.toString)(10) shouldBe "10"
+  }
+  
+  test("scala high order function api") {
+    val list = List(1, 2, 3)
+    list.map(_ + 10) shouldBe List(11, 12, 13)
+    list.reduce(_ + _) shouldBe 6
+    list.fold(1)(_ * _) shouldBe 6
+    list.flatMap(x => List(List(x * 11), List(x * 11 + 1))).flatten shouldBe List(11, 12, 22, 23, 33, 34) 
+    // flatMap = map + flatten
+  }
 }
